414b4f4958fdbb7519fa6ec4aa11e7cdeba02069
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-05-10 11:22:20 -0700

    Controls - Conditional Experimental Mode - Curve Detected Ahead
    Switch to 'Experimental Mode' when a curve is detected.

--- selfdrive/frogpilot/controls/lib/conditional_experimental_mode.py
+++ selfdrive/frogpilot/controls/lib/conditional_experimental_mode.py
@@ -8,12 +8,14 @@ class ConditionalExperimentalMode:
   def __init__(self):
     self.params_memory = Params("/dev/shm/params")
 
+    self.curve_detected = False
     self.experimental_mode = False
     self.lead_detected = False
 
     self.previous_status_value = 0
     self.status_value = 0
 
+    self.curvature_mac = MovingAverageCalculator()
     self.lead_detection_mac = MovingAverageCalculator()
 
   def update(self, carState, enabled, frogpilotNavigation, modelData, radarState, road_curvature, t_follow, v_ego, frogpilot_toggles):
@@ -44,11 +46,29 @@ class ConditionalExperimentalMode:
       self.status_value = 10 if self.lead_detected else 11
       return True
 
+    if frogpilot_toggles.conditional_curves and self.curve_detected:
+      self.status_value = 14
+      return True
+
     return False
 
   def update_conditions(self, lead_distance, lead_status, modelData, road_curvature, standstill, t_follow, v_ego, v_lead, frogpilot_toggles):
     self.lead_detection(lead_status)
+    self.road_curvature(road_curvature, v_ego, frogpilot_toggles)
 
   def lead_detection(self, lead_status):
     self.lead_detection_mac.add_data(lead_status)
     self.lead_detected = self.lead_detection_mac.get_moving_average() >= PROBABILITY
+
+  def road_curvature(self, road_curvature, v_ego, frogpilot_toggles):
+    lead_check = frogpilot_toggles.conditional_curves_lead or not self.lead_detected
+
+    if lead_check:
+      curve_detected = (1 / road_curvature)**0.5 < v_ego
+      curve_active = (0.75 / road_curvature)**0.5 < v_ego and self.curve_detected
+
+      self.curvature_mac.add_data(curve_detected or curve_active)
+      self.curve_detected = self.curvature_mac.get_moving_average() >= PROBABILITY
+    else:
+      self.curvature_mac.reset_data()
+      self.curve_detected = False
