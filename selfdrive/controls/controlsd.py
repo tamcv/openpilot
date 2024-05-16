52e946e7dae8d16b9651f94512565c3162a905f7
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-05-16 02:18:42 -0700

    Controls - Experimental Mode Activation - Long Press Distance
    Enable/disable 'Experimental Mode' by holding down the 'distance' button on your steering wheel for 0.5 seconds.

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -683,7 +683,7 @@ class Controls:
     # decrement personality on distance button press
     if self.CP.openpilotLongitudinalControl:
       if any(not be.pressed and be.type == ButtonType.gapAdjustCruise for be in CS.buttonEvents) or self.onroad_distance_pressed:
-        if self.params_memory.get_bool("OnroadDistanceButtonPressed")):
+        if not (self.sm['frogpilotCarState'].distanceLongPressed or self.params_memory.get_bool("OnroadDistanceButtonPressed")):
           self.personality = (self.personality - 1) % 3
           self.params.put_nonblocking('LongitudinalPersonality', str(self.personality))
       self.onroad_distance_pressed = self.params_memory.get_bool("OnroadDistanceButtonPressed")
