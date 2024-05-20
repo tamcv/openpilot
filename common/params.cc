nessied/mazda3_2018

FrogPilot setup - Setup toggles

common/params.cc
@@ -207,6 +207,256 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"UpdaterTargetBranch", CLEAR_ON_MANAGER_START},
     {"UpdaterLastFetchTime", PERSISTENT},
     {"Version", PERSISTENT},
+
+    // FrogPilot parameters
+    {"AccelerationPath", PERSISTENT},
+    {"AccelerationProfile", PERSISTENT},
+    {"AdjacentPath", PERSISTENT},
+    {"AdjacentPathMetrics", PERSISTENT},
+    {"AggressiveAcceleration", PERSISTENT},
+    {"AggressiveAccelerationExperimental", PERSISTENT},
+    {"AggressiveFollow", PERSISTENT},
+    {"AggressiveJerk", PERSISTENT},
+    {"AlertVolumeControl", PERSISTENT},
+    {"AlwaysOnLateral", PERSISTENT},
+    {"AlwaysOnLateralMain", PERSISTENT},
+    {"AMapKey1", PERSISTENT},
+    {"AMapKey2", PERSISTENT},
+    {"ApiCache_DriveStats", PERSISTENT},
+    {"AutomaticUpdates", PERSISTENT},
+    {"AvailableModels", PERSISTENT},
+    {"AvailableModelsNames", PERSISTENT},
+    {"BorderMetrics", PERSISTENT},
+    {"BigMap", PERSISTENT},
+    {"BlindSpotMetrics", PERSISTENT},
+    {"BlindSpotPath", PERSISTENT},
+    {"CameraFPS", PERSISTENT},
+    {"CameraView", PERSISTENT},
+    {"CameraViewReset", PERSISTENT},
+    {"CarMake", PERSISTENT},
+    {"CarModel", PERSISTENT},
+    {"CECurves", PERSISTENT},
+    {"CECurvesLead", PERSISTENT},
+    {"CENavigation", PERSISTENT},
+    {"CENavigationIntersections", PERSISTENT},
+    {"CENavigationLead", PERSISTENT},
+    {"CENavigationTurns", PERSISTENT},
+    {"CESignal", PERSISTENT},
+    {"CESlowerLead", PERSISTENT},
+    {"CESpeed", PERSISTENT},
+    {"CESpeedLead", PERSISTENT},
+    {"CEStatus", PERSISTENT},
+    {"CEStopLights", PERSISTENT},
+    {"CEStopLightsLead", PERSISTENT},
+    {"ClusterOffset", PERSISTENT},
+    {"Compass", PERSISTENT},
+    {"ConditionalExperimental", PERSISTENT},
+    {"CrosstrekTorque", PERSISTENT},
+    {"CurrentHolidayTheme", PERSISTENT},
+    {"CurrentRandomEvent", PERSISTENT},
+    {"CurveSensitivity", PERSISTENT},
+    {"CustomAlerts", PERSISTENT},
+    {"CustomColors", PERSISTENT},
+    {"CustomCruise", PERSISTENT},
+    {"CustomCruiseLong", PERSISTENT},
+    {"CustomIcons", PERSISTENT},
+    {"CustomPaths", PERSISTENT},
+    {"CustomPersonalities", PERSISTENT},
+    {"CustomUI", PERSISTENT},
+    {"CustomSignals", PERSISTENT},
+    {"CustomSounds", PERSISTENT},
+    {"CustomTheme", PERSISTENT},
+    {"CydiaTune", PERSISTENT},
+    {"DecelerationProfile", PERSISTENT},
+    {"DeveloperUI", PERSISTENT},
+    {"DeviceManagement", PERSISTENT},
+    {"DeviceShutdown", PERSISTENT},
+    {"DisableMTSCSmoothing", PERSISTENT},
+    {"DisableOnroadUploads", PERSISTENT},
+    {"DisableOpenpilotLongitudinal", PERSISTENT},
+    {"DisableVTSCSmoothing", PERSISTENT},
+    {"DisengageVolume", PERSISTENT},
+    {"DistanceLongPressed", PERSISTENT},
+    {"DragonPilotTune", PERSISTENT},
+    {"DriverCamera", PERSISTENT},
+    {"DynamicPathWidth", PERSISTENT},
+    {"EngageVolume", PERSISTENT},
+    {"ExperimentalModeActivation", PERSISTENT},
+    {"ExperimentalModeViaDistance", PERSISTENT},
+    {"ExperimentalModeViaLKAS", PERSISTENT},
+    {"ExperimentalModeViaTap", PERSISTENT},
+    {"Fahrenheit", PERSISTENT},
+    {"FingerprintLogged", CLEAR_ON_MANAGER_START},
+    {"ForceAutoTune", PERSISTENT},
+    {"ForceFingerprint", PERSISTENT},
+    {"ForceMPHDashboard", PERSISTENT},
+    {"FPSCounter", PERSISTENT},
+    {"FrogPilotDrives", PERSISTENT},
+    {"FrogPilotKilometers", PERSISTENT},
+    {"FrogPilotMinutes", PERSISTENT},
+    {"FrogPilotTogglesUpdated", PERSISTENT},
+    {"FrogsGoMoo", PERSISTENT},
+    {"FrogsGoMooTune", PERSISTENT},
+    {"FullMap", PERSISTENT},
+    {"GasRegenCmd", PERSISTENT},
+    {"GMapKey", PERSISTENT},
+    {"GoatScream", PERSISTENT},
+    {"GreenLightAlert", PERSISTENT},
+    {"HideAlerts", PERSISTENT},
+    {"HideAOLStatusBar", PERSISTENT},
+    {"HideCEMStatusBar", PERSISTENT},
+    {"HideDisableOpenpilotLongitudinal", PERSISTENT},
+    {"HideLeadMarker", PERSISTENT},
+    {"HideMapIcon", PERSISTENT},
+    {"HideMaxSpeed", PERSISTENT},
+    {"HideSpeed", PERSISTENT},
+    {"HideSpeedUI", PERSISTENT},
+    {"HideUIElements", PERSISTENT},
+    {"HolidayThemes", PERSISTENT},
+    {"IncreaseThermalLimits", PERSISTENT},
+    {"KaofuiIcons", PERSISTENT},
+    {"LaneChangeTime", PERSISTENT},
+    {"LaneDetectionWidth", PERSISTENT},
+    {"LaneLinesWidth", PERSISTENT},
+    {"LastMapsUpdate", PERSISTENT},
+    {"LateralMetrics", PERSISTENT},
+    {"LateralTune", PERSISTENT},
+    {"LeadDepartingAlert", PERSISTENT},
+    {"LeadDetectionThreshold", PERSISTENT},
+    {"LockDoors", PERSISTENT},
+    {"LongitudinalMetrics", PERSISTENT},
+    {"LongitudinalTune", PERSISTENT},
+    {"LongPitch", PERSISTENT},
+    {"LoudBlindspotAlert", PERSISTENT},
+    {"LowVoltageShutdown", PERSISTENT},
+    {"ManualUpdateInitiated", PERSISTENT},
+    {"MapboxPublicKey", PERSISTENT},
+    {"MapboxSecretKey", PERSISTENT},
+    {"MapAcceleration", PERSISTENT},
+    {"MapDeceleration", PERSISTENT},
+    {"MapGears", PERSISTENT},
+    {"MapsSelected", PERSISTENT},
+    {"MapSpeedLimit", PERSISTENT},
+    {"MapStyle", PERSISTENT},
+    {"MapTargetLatA", PERSISTENT},
+    {"MapTargetVelocities", PERSISTENT},
+    {"Model", PERSISTENT},
+    {"ModelDownloadProgress", PERSISTENT},
+    {"ModelName", PERSISTENT},
+    {"ModelSelector", PERSISTENT},
+    {"ModelToDownload", PERSISTENT},
+    {"ModelUI", PERSISTENT},
+    {"MTSCAggressiveness", PERSISTENT},
+    {"MTSCCurvatureCheck", PERSISTENT},
+    {"MTSCEnabled", PERSISTENT},
+    {"NNFF", PERSISTENT},
+    {"NNFFLite", PERSISTENT},
+    {"NNFFModelName", PERSISTENT},
+    {"NoLogging", PERSISTENT},
+    {"NoUploads", PERSISTENT},
+    {"NudgelessLaneChange", PERSISTENT},
+    {"NumericalTemp", PERSISTENT},
+    {"OfflineMode", PERSISTENT},
+    {"Offset1", PERSISTENT},
+    {"Offset2", PERSISTENT},
+    {"Offset3", PERSISTENT},
+    {"Offset4", PERSISTENT},
+    {"OneLaneChange", PERSISTENT},
+    {"OnroadDistanceButton", PERSISTENT},
+    {"OnroadDistanceButtonPressed", PERSISTENT},
+    {"OSMDownloadBounds", PERSISTENT},
+    {"OSMDownloadLocations", PERSISTENT},
+    {"OSMDownloadProgress", CLEAR_ON_MANAGER_START},
+    {"PathEdgeWidth", PERSISTENT},
+    {"PathWidth", PERSISTENT},
+    {"PauseAOLOnBrake", PERSISTENT},
+    {"PauseLateralOnSignal", PERSISTENT},
+    {"PauseLateralSpeed", PERSISTENT},
+    {"PedalsOnUI", PERSISTENT},
+    {"PreferredSchedule", PERSISTENT},
+    {"PreviousSpeedLimit", PERSISTENT},
+    {"PromptDistractedVolume", PERSISTENT},
+    {"PromptVolume", PERSISTENT},
+    {"QOLControls", PERSISTENT},
+    {"QOLVisuals", PERSISTENT},
+    {"RandomEvents", PERSISTENT},
+    {"RefuseVolume", PERSISTENT},
+    {"RelaxedFollow", PERSISTENT},
+    {"RelaxedJerk", PERSISTENT},
+    {"ResetSteerRatio", PERSISTENT},
+    {"ReverseCruise", PERSISTENT},
+    {"ReverseCruiseUI", PERSISTENT},
+    {"RoadEdgesWidth", PERSISTENT},
+    {"RoadName", PERSISTENT},
+    {"RoadNameUI", PERSISTENT},
+    {"RotatingWheel", PERSISTENT},
+    {"SchedulePending", PERSISTENT},
+    {"ScreenBrightness", PERSISTENT},
+    {"ScreenBrightnessOnroad", PERSISTENT},
+    {"ScreenManagement", PERSISTENT},
+    {"ScreenRecorder", PERSISTENT},
+    {"ScreenTimeout", PERSISTENT},
+    {"ScreenTimeoutOnroad", PERSISTENT},
+    {"SearchInput", PERSISTENT},
+    {"SetSpeedLimit", PERSISTENT},
+    {"SetSpeedOffset", PERSISTENT},
+    {"ShowCPU", PERSISTENT},
+    {"ShowGPU", PERSISTENT},
+    {"ShowIP", PERSISTENT},
+    {"ShowMemoryUsage", PERSISTENT},
+    {"ShowSLCOffset", PERSISTENT},
+    {"ShowSLCOffsetUI", PERSISTENT},
+    {"ShowSteering", PERSISTENT},
+    {"ShowStorageLeft", PERSISTENT},
+    {"ShowStorageUsed", PERSISTENT},
+    {"Sidebar", PERSISTENT},
+    {"SidebarMetrics", PERSISTENT},
+    {"SignalMetrics", PERSISTENT},
+    {"SLCConfirmation", PERSISTENT},
+    {"SLCConfirmationLower", PERSISTENT},
+    {"SLCConfirmationHigher", PERSISTENT},
+    {"SLCConfirmed", PERSISTENT},
+    {"SLCConfirmedPressed", PERSISTENT},
+    {"SLCLookaheadHigher", PERSISTENT},
+    {"SLCLookaheadLower", PERSISTENT},
+    {"SLCFallback", PERSISTENT},
+    {"SLCOverride", PERSISTENT},
+    {"SLCPriority", PERSISTENT},
+    {"SLCPriority1", PERSISTENT},
+    {"SLCPriority2", PERSISTENT},
+    {"SLCPriority3", PERSISTENT},
+    {"SmoothBraking", PERSISTENT},
+    {"SmoothBrakingFarLead", PERSISTENT},
+    {"SmoothBrakingJerk", PERSISTENT},
+    {"SNGHack", PERSISTENT},
+    {"SpeedLimitController", PERSISTENT},
+    {"SpeedLimitChangedAlert", PERSISTENT},
+    {"StandardFollow", PERSISTENT},
+    {"StandardJerk", PERSISTENT},
+    {"StandbyMode", PERSISTENT},
+    {"SteerRatio", PERSISTENT},
+    {"SteerRatioStock", PERSISTENT},
+    {"StockTune", PERSISTENT},
+    {"StoppingDistance", PERSISTENT},
+    {"TacoTune", PERSISTENT},
+    {"TetheringEnabled", PERSISTENT},
+    {"ToyotaDoors", PERSISTENT},
+    {"TrafficFollow", PERSISTENT},
+    {"TrafficJerk", PERSISTENT},
+    {"TrafficMode", PERSISTENT},
+    {"TrafficModeActive", CLEAR_ON_OFFROAD_TRANSITION},
+    {"TurnAggressiveness", PERSISTENT},
+    {"TurnDesires", PERSISTENT},
+    {"UnlimitedLength", PERSISTENT},
+    {"UnlockDoors", PERSISTENT},
+    {"Updated", PERSISTENT},
+    {"UseSI", PERSISTENT},
+    {"UseVienna", PERSISTENT},
+    {"VisionTurnControl", PERSISTENT},
+    {"WarningImmediateVolume", PERSISTENT},
+    {"WarningSoftVolume", PERSISTENT},
+    {"WheelIcon", PERSISTENT},
+    {"WheelSpeed", PERSISTENT},
 };
 
 } // namespace

tamcv/openpilot

common/params.cc
@@ -209,7 +209,265 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"Version", PERSISTENT},
 
     // FrogPilot parameters
+    {"AccelerationPath", PERSISTENT},
+    {"AccelerationProfile", PERSISTENT},
+    {"AdjacentPath", PERSISTENT},
+    {"AdjacentPathMetrics", PERSISTENT},
+    {"AggressiveAcceleration", PERSISTENT},
+    {"AggressiveAccelerationExperimental", PERSISTENT},
+    {"AggressiveFollow", PERSISTENT},
+    {"AggressiveJerkAcceleration", PERSISTENT},
+    {"AggressiveJerkSpeed", PERSISTENT},
+    {"AggressivePersonalityProfile", PERSISTENT},
+    {"AlertVolumeControl", PERSISTENT},
+    {"AlwaysOnLateral", PERSISTENT},
+    {"AlwaysOnLateralMain", PERSISTENT},
+    {"AMapKey1", PERSISTENT},
+    {"AMapKey2", PERSISTENT},
+    {"ApiCache_DriveStats", PERSISTENT},
+    {"AutomaticUpdates", PERSISTENT},
+    {"AvailableModels", PERSISTENT},
+    {"AvailableModelsNames", PERSISTENT},
+    {"BorderMetrics", PERSISTENT},
+    {"BigMap", PERSISTENT},
+    {"BlindSpotMetrics", PERSISTENT},
+    {"BlindSpotPath", PERSISTENT},
+    {"CameraFPS", PERSISTENT},
+    {"CameraView", PERSISTENT},
+    {"CameraViewReset", PERSISTENT},
+    {"CarMake", PERSISTENT},
+    {"CarModel", PERSISTENT},
+    {"CECurves", PERSISTENT},
+    {"CECurvesLead", PERSISTENT},
+    {"CENavigation", PERSISTENT},
+    {"CENavigationIntersections", PERSISTENT},
+    {"CENavigationLead", PERSISTENT},
+    {"CENavigationTurns", PERSISTENT},
+    {"CESignal", PERSISTENT},
+    {"CESlowerLead", PERSISTENT},
+    {"CESpeed", PERSISTENT},
+    {"CESpeedLead", PERSISTENT},
+    {"CEStatus", PERSISTENT},
+    {"CEStopLights", PERSISTENT},
+    {"CEStopLightsLead", PERSISTENT},
+    {"ClusterOffset", PERSISTENT},
+    {"Compass", PERSISTENT},
+    {"ConditionalExperimental", PERSISTENT},
+    {"CrosstrekTorque", PERSISTENT},
+    {"CurrentHolidayTheme", PERSISTENT},
+    {"CurrentRandomEvent", PERSISTENT},
+    {"CurveSensitivity", PERSISTENT},
+    {"CustomAlerts", PERSISTENT},
+    {"CustomColors", PERSISTENT},
+    {"CustomCruise", PERSISTENT},
+    {"CustomCruiseLong", PERSISTENT},
+    {"CustomIcons", PERSISTENT},
+    {"CustomPaths", PERSISTENT},
+    {"CustomPersonalities", PERSISTENT},
+    {"CustomUI", PERSISTENT},
+    {"CustomSignals", PERSISTENT},
+    {"CustomSounds", PERSISTENT},
+    {"CustomTheme", PERSISTENT},
+    {"CydiaTune", PERSISTENT},
+    {"DecelerationProfile", PERSISTENT},
+    {"DeveloperUI", PERSISTENT},
+    {"DeviceManagement", PERSISTENT},
+    {"DeviceShutdown", PERSISTENT},
+    {"DisableMTSCSmoothing", PERSISTENT},
+    {"DisableOnroadUploads", PERSISTENT},
+    {"DisableOpenpilotLongitudinal", PERSISTENT},
+    {"DisableVTSCSmoothing", PERSISTENT},
+    {"DisengageVolume", PERSISTENT},
+    {"DistanceLongPressed", PERSISTENT},
+    {"DragonPilotTune", PERSISTENT},
+    {"DriverCamera", PERSISTENT},
+    {"DrivingPersonalities", PERSISTENT},
+    {"DynamicPathWidth", PERSISTENT},
+    {"DynamicPedalsOnUI", PERSISTENT},
+    {"EngageVolume", PERSISTENT},
+    {"ExperimentalModeActivation", PERSISTENT},
+    {"ExperimentalModeViaDistance", PERSISTENT},
+    {"ExperimentalModeViaLKAS", PERSISTENT},
+    {"ExperimentalModeViaTap", PERSISTENT},
+    {"Fahrenheit", PERSISTENT},
+    {"FingerprintLogged", CLEAR_ON_MANAGER_START},
+    {"ForceAutoTune", PERSISTENT},
+    {"ForceFingerprint", PERSISTENT},
+    {"ForceMPHDashboard", PERSISTENT},
+    {"FPSCounter", PERSISTENT},
+    {"FrogPilotDrives", PERSISTENT},
+    {"FrogPilotKilometers", PERSISTENT},
+    {"FrogPilotMinutes", PERSISTENT},
+    {"FrogPilotTogglesUpdated", PERSISTENT},
+    {"FrogsGoMoo", PERSISTENT},
+    {"FrogsGoMooTune", PERSISTENT},
+    {"FullMap", PERSISTENT},
+    {"GasRegenCmd", PERSISTENT},
+    {"GMapKey", PERSISTENT},
+    {"GoatScream", PERSISTENT},
+    {"GreenLightAlert", PERSISTENT},
+    {"HideAlerts", PERSISTENT},
+    {"HideAOLStatusBar", PERSISTENT},
+    {"HideCEMStatusBar", PERSISTENT},
+    {"HideDisableOpenpilotLongitudinal", PERSISTENT},
+    {"HideLeadMarker", PERSISTENT},
+    {"HideMapIcon", PERSISTENT},
+    {"HideMaxSpeed", PERSISTENT},
+    {"HideSpeed", PERSISTENT},
+    {"HideSpeedUI", PERSISTENT},
+    {"HideUIElements", PERSISTENT},
+    {"HolidayThemes", PERSISTENT},
+    {"IncreaseThermalLimits", PERSISTENT},
+    {"KaofuiIcons", PERSISTENT},
+    {"LaneChangeTime", PERSISTENT},
+    {"LaneDetectionWidth", PERSISTENT},
+    {"LaneLinesWidth", PERSISTENT},
+    {"LastMapsUpdate", PERSISTENT},
+    {"LateralMetrics", PERSISTENT},
+    {"LateralTune", PERSISTENT},
+    {"LeadDepartingAlert", PERSISTENT},
+    {"LeadDetectionThreshold", PERSISTENT},
+    {"LockDoors", PERSISTENT},
+    {"LongitudinalMetrics", PERSISTENT},
+    {"LongitudinalTune", PERSISTENT},
+    {"LongPitch", PERSISTENT},
+    {"LoudBlindspotAlert", PERSISTENT},
+    {"LowVoltageShutdown", PERSISTENT},
+    {"ManualUpdateInitiated", PERSISTENT},
+    {"MapboxPublicKey", PERSISTENT},
+    {"MapboxSecretKey", PERSISTENT},
+    {"MapAcceleration", PERSISTENT},
+    {"MapDeceleration", PERSISTENT},
+    {"MapGears", PERSISTENT},
+    {"MapsSelected", PERSISTENT},
+    {"MapSpeedLimit", PERSISTENT},
+    {"MapStyle", PERSISTENT},
+    {"MapTargetLatA", PERSISTENT},
+    {"MapTargetVelocities", PERSISTENT},
+    {"Model", PERSISTENT},
+    {"ModelDownloadProgress", PERSISTENT},
+    {"ModelName", PERSISTENT},
+    {"ModelSelector", PERSISTENT},
+    {"ModelToDownload", PERSISTENT},
+    {"ModelUI", PERSISTENT},
+    {"MTSCAggressiveness", PERSISTENT},
+    {"MTSCCurvatureCheck", PERSISTENT},
+    {"MTSCEnabled", PERSISTENT},
+    {"NNFF", PERSISTENT},
+    {"NNFFLite", PERSISTENT},
+    {"NNFFModelName", PERSISTENT},
+    {"NoLogging", PERSISTENT},
+    {"NoUploads", PERSISTENT},
+    {"NudgelessLaneChange", PERSISTENT},
+    {"NumericalTemp", PERSISTENT},
+    {"OfflineMode", PERSISTENT},
+    {"Offset1", PERSISTENT},
+    {"Offset2", PERSISTENT},
+    {"Offset3", PERSISTENT},
+    {"Offset4", PERSISTENT},
+    {"OneLaneChange", PERSISTENT},
+    {"OnroadDistanceButton", PERSISTENT},
+    {"OnroadDistanceButtonPressed", PERSISTENT},
+    {"OSMDownloadBounds", PERSISTENT},
+    {"OSMDownloadLocations", PERSISTENT},
+    {"OSMDownloadProgress", CLEAR_ON_MANAGER_START},
     {"ParamConversionVersion", PERSISTENT},
+    {"PathEdgeWidth", PERSISTENT},
+    {"PathWidth", PERSISTENT},
+    {"PauseAOLOnBrake", PERSISTENT},
+    {"PauseLateralOnSignal", PERSISTENT},
+    {"PauseLateralSpeed", PERSISTENT},
+    {"PedalsOnUI", PERSISTENT},
+    {"PreferredSchedule", PERSISTENT},
+    {"PreviousSpeedLimit", PERSISTENT},
+    {"PromptDistractedVolume", PERSISTENT},
+    {"PromptVolume", PERSISTENT},
+    {"QOLControls", PERSISTENT},
+    {"QOLVisuals", PERSISTENT},
+    {"RandomEvents", PERSISTENT},
+    {"RefuseVolume", PERSISTENT},
+    {"RelaxedFollow", PERSISTENT},
+    {"RelaxedJerkAcceleration", PERSISTENT},
+    {"RelaxedJerkSpeed", PERSISTENT},
+    {"RelaxedPersonalityProfile", PERSISTENT},
+    {"ResetSteerRatio", PERSISTENT},
+    {"ReverseCruise", PERSISTENT},
+    {"ReverseCruiseUI", PERSISTENT},
+    {"RoadEdgesWidth", PERSISTENT},
+    {"RoadName", PERSISTENT},
+    {"RoadNameUI", PERSISTENT},
+    {"RotatingWheel", PERSISTENT},
+    {"ScreenBrightness", PERSISTENT},
+    {"ScreenBrightnessOnroad", PERSISTENT},
+    {"ScreenManagement", PERSISTENT},
+    {"ScreenRecorder", PERSISTENT},
+    {"ScreenTimeout", PERSISTENT},
+    {"ScreenTimeoutOnroad", PERSISTENT},
+    {"SearchInput", PERSISTENT},
+    {"SetSpeedLimit", PERSISTENT},
+    {"SetSpeedOffset", PERSISTENT},
+    {"ShowCPU", PERSISTENT},
+    {"ShowGPU", PERSISTENT},
+    {"ShowIP", PERSISTENT},
+    {"ShowMemoryUsage", PERSISTENT},
+    {"ShowSLCOffset", PERSISTENT},
+    {"ShowSLCOffsetUI", PERSISTENT},
+    {"ShowSteering", PERSISTENT},
+    {"ShowStorageLeft", PERSISTENT},
+    {"ShowStorageUsed", PERSISTENT},
+    {"Sidebar", PERSISTENT},
+    {"SidebarMetrics", PERSISTENT},
+    {"SignalMetrics", PERSISTENT},
+    {"SLCConfirmation", PERSISTENT},
+    {"SLCConfirmationLower", PERSISTENT},
+    {"SLCConfirmationHigher", PERSISTENT},
+    {"SLCConfirmed", PERSISTENT},
+    {"SLCConfirmedPressed", PERSISTENT},
+    {"SLCLookaheadHigher", PERSISTENT},
+    {"SLCLookaheadLower", PERSISTENT},
+    {"SLCFallback", PERSISTENT},
+    {"SLCOverride", PERSISTENT},
+    {"SLCPriority", PERSISTENT},
+    {"SLCPriority1", PERSISTENT},
+    {"SLCPriority2", PERSISTENT},
+    {"SLCPriority3", PERSISTENT},
+    {"SmoothBraking", PERSISTENT},
+    {"SmoothBrakingFarLead", PERSISTENT},
+    {"SmoothBrakingJerk", PERSISTENT},
+    {"SNGHack", PERSISTENT},
+    {"SpeedLimitController", PERSISTENT},
+    {"SpeedLimitChangedAlert", PERSISTENT},
+    {"StandardFollow", PERSISTENT},
+    {"StandardJerkAcceleration", PERSISTENT},
+    {"StandardJerkSpeed", PERSISTENT},
+    {"StandardPersonalityProfile", PERSISTENT},
+    {"StandbyMode", PERSISTENT},
+    {"StaticPedalsOnUI", PERSISTENT},
+    {"SteerRatio", PERSISTENT},
+    {"SteerRatioStock", PERSISTENT},
+    {"StockTune", PERSISTENT},
+    {"StoppingDistance", PERSISTENT},
+    {"TacoTune", PERSISTENT},
+    {"TetheringEnabled", PERSISTENT},
+    {"ToyotaDoors", PERSISTENT},
+    {"TrafficFollow", PERSISTENT},
+    {"TrafficJerkAcceleration", PERSISTENT},
+    {"TrafficJerkSpeed", PERSISTENT},
+    {"TrafficMode", PERSISTENT},
+    {"TrafficModeActive", CLEAR_ON_OFFROAD_TRANSITION},
+    {"TrafficPersonalityProfile", PERSISTENT},
+    {"TurnAggressiveness", PERSISTENT},
+    {"TurnDesires", PERSISTENT},
+    {"UnlimitedLength", PERSISTENT},
+    {"UnlockDoors", PERSISTENT},
+    {"Updated", PERSISTENT},
+    {"UseSI", PERSISTENT},
+    {"UseVienna", PERSISTENT},
+    {"VisionTurnControl", PERSISTENT},
+    {"WarningImmediateVolume", PERSISTENT},
+    {"WarningSoftVolume", PERSISTENT},
+    {"WheelIcon", PERSISTENT},
+    {"WheelSpeed", PERSISTENT},
 };
 
 } // namespace